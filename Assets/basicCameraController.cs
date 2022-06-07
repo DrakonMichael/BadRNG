c3cd1bfc3666814ad6a3e30003217aa71c8d4881
DrakonMichael <mpkarpov@gmail.com>
2022-06-06 21:16:05 -0500

    Grid placement version 1

--- Assets/basicCameraController.cs
+++ Assets/basicCameraController.cs
@@ -21,12 +21,30 @@ public class basicCameraController : MonoBehaviour
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
+    private bool dragging = false;
+
     void Update()
     {
         lastMouse = Input.mousePosition - lastMouse;
         lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
         lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
+        if(Input.GetMouseButtonDown(1))
+        {
+            dragging = true;
+            
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            dragging = false;
+
+        }
+
+        if (dragging)
+        {
+            transform.eulerAngles = lastMouse;
+        }
+        
         lastMouse = Input.mousePosition;
         //Mouse  camera angle done.  
 
--- Assets/placementHandler.cs
+++ Assets/placementHandler.cs
@@ -5,23 +5,63 @@ using Mirror;
 
 public class placementHandler : NetworkBehaviour
 {
-    public GameObject tile;
+    public GameObject[] tiles;
+    public int[] tileType;
+    public GameObject selector;
 
+    private Plane gridPlane;
+    private int selected = 0;
 
+    private void Start()
+    {
+        gridPlane = new Plane(Vector3.up, 0);
+    }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0.0f;
+        Vector3 newVector = new Vector3(0,0,0);
+
+        if (gridPlane.Raycast(ray, out enter))
         {
-            Debug.Log("piss and shit");
+
+            Vector3 hitLocation = ray.GetPoint(enter);
+            newVector = new Vector3(Mathf.Ceil(hitLocation.x) - 0.5f, 0.1f, Mathf.Ceil(hitLocation.z) - 0.5f);
+            selector.transform.position = newVector;
+
+            Vector3 cornerVector = hitLocation - newVector;
+            float angle = Mathf.Round(Mathf.Atan2(cornerVector.x, cornerVector.z)/(Mathf.PI / 2)) * 90;
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(tileType[selected] == 0)
+                {
+                    placeSelectedTile(newVector, Quaternion.Euler(0, 0, 0));
+                } else
+                {
+                    placeSelectedTile(newVector, Quaternion.Euler(0, angle-90, 0));
+                }
+                
+            }
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            selected++;
+            selected = selected % tiles.Length;
         }
     }
 
-    private void placeSelectedTile(Vector3 location)
+    private void placeSelectedTile(Vector3 location, Quaternion orientation)
     {
         if (isServer)
         {
-            GameObject instantiatedTile = GameObject.Instantiate(tile, location, Quaternion.Euler(0, 0, 0));
+            GameObject instantiatedTile = GameObject.Instantiate(tiles[selected], location, orientation);
             NetworkServer.Spawn(instantiatedTile);
         }
     }
