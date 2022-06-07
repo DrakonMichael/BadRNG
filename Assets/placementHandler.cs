df69a73cf0614d4b2d01bf1fe45f397368ad1d55
DrakonMichael <mpkarpov@gmail.com>
2022-06-06 23:41:37 -0500

    Placement indicator added

--- Assets/placementHandler.cs
+++ Assets/placementHandler.cs
@@ -7,14 +7,28 @@ public class placementHandler : NetworkBehaviour
 {
     public GameObject[] tiles;
     public int[] tileType;
-    public GameObject selector;
+    public Material selectionMaterial;
 
+    private GameObject selector;
     private Plane gridPlane;
     private int selected = 0;
 
     private void Start()
     {
         gridPlane = new Plane(Vector3.up, 0);
+        loadNewSelector(0);
+    }
+
+    private void loadNewSelector(int numberToLoad)
+    {
+        if(selector) { Destroy(selector); }
+        
+        selector = GameObject.Instantiate(tiles[numberToLoad], new Vector3(0, -10, 0), Quaternion.Euler(0, 0, 0));
+        foreach (Renderer r in selector.GetComponentsInChildren<Renderer>())
+        {
+            r.material = selectionMaterial;
+        }
+        
     }
 
     private void Update()
@@ -29,11 +43,11 @@ public class placementHandler : NetworkBehaviour
 
             Vector3 hitLocation = ray.GetPoint(enter);
             newVector = new Vector3(Mathf.Ceil(hitLocation.x) - 0.5f, 0.1f, Mathf.Ceil(hitLocation.z) - 0.5f);
-            selector.transform.position = newVector;
-
+            
             Vector3 cornerVector = hitLocation - newVector;
             float angle = Mathf.Round(Mathf.Atan2(cornerVector.x, cornerVector.z)/(Mathf.PI / 2)) * 90;
 
+            setIndicatorLocation(newVector, angle, tileType[selected]);
 
             if (Input.GetMouseButtonDown(0))
             {
@@ -54,7 +68,18 @@ public class placementHandler : NetworkBehaviour
         {
             selected++;
             selected = selected % tiles.Length;
+            loadNewSelector(selected);
         }
+    }
+
+    private void setIndicatorLocation(Vector3 location, float angle, int tileType)
+    {
+        selector.transform.position = location;
+        if (tileType == 1)
+        {
+            selector.transform.rotation = Quaternion.Euler(0, angle - 90, 0);
+        } 
+        
     }
 
     private void placeSelectedTile(Vector3 location, Quaternion orientation)
